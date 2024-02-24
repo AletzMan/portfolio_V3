46ec9deb9d5154cdeff9762dc54fdf8ed0fd10f0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-02-23 23:53:27 -0600

    Add i18n es and en

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
+    i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+        routing: {
+            prefixDefaultLocale: false
+        }
+    }
 })

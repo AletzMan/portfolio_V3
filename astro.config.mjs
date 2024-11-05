104dcd99252737869f05ebbe6e042feb951c8691
= <=>
2024-11-04 23:38:34 -0600

    Add adapter vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['es', 'en'],
-        routing: {
-            prefixDefaultLocale: false
-        }
-    },
-    output: "hybrid"
-})
+  integrations: [react()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  output: "hybrid",
+  adapter: vercel()
+});
